af4e7c50ce5e9ebba9205f3fdd4ee9ed171b6676
smilingjb12 <smilingjb12@gmail.com>
2015-08-22 00:59:14 +0300

    proper minification syntax for directives

--- GRSS.Web/App/shared/directives/categorySelect.js
+++ GRSS.Web/App/shared/directives/categorySelect.js
@@ -12,12 +12,14 @@
                 ngModel: '=',
                 remove: '&onRemove'
             },
-            controller: function ($scope) {
-                $scope.verticalMarkets = [{ id: 1, name: "vm1" }, { id: 2, name: "vm2" }, { id: 3, name: "vm3" }];
-                $scope.businessUnits = [{ id: 1, name: "bu1" }, { id: 2, name: "bu2" }, { id: 3, name: "bu3" }];
-                $scope.categories = [{ id: 1, name: "c1" }, { id: 2, name: "c2" }, { id: 3, name: "c3" }];
-            },
+            controller: ['$scope', CategorySelectCtrl],
             templateUrl: appSettings.templatesFolderPath + 'category-select.cshtml'
         };
+    }
+
+    function CategorySelectCtrl($scope) {
+        $scope.verticalMarkets = [{ id: 1, name: "vm1" }, { id: 2, name: "vm2" }, { id: 3, name: "vm3" }];
+        $scope.businessUnits = [{ id: 1, name: "bu1" }, { id: 2, name: "bu2" }, { id: 3, name: "bu3" }];
+        $scope.categories = [{ id: 1, name: "c1" }, { id: 2, name: "c2" }, { id: 3, name: "c3" }];
     }
 })();
--- GRSS.Web/App/shared/directives/imageUpload.js
+++ GRSS.Web/App/shared/directives/imageUpload.js
@@ -10,23 +10,23 @@
             scope: {
                 imageSource: '='
             },
-            link: function($scope, element, attrs) {
-                $scope.fileChanged = function(elem) {
+            link: function(scope, element, attrs) {
+                scope.fileChanged = function(elem) {
                     var reader = new FileReader();
                     reader.onload = function(e) {
-                        $scope.$apply(function() {
-                            $scope.imageSource = e.target.result;
+                        scope.$apply(function() {
+                            scope.imageSource = e.target.result;
                         });
                     };
                     reader.readAsDataURL(elem.files[0]);
                 };
 
-                $scope.selectFile = function () {
+                scope.selectFile = function () {
                     angular.element(element).find(".image-file-input").trigger('click');
                 };
 
-                $scope.imagePreviewSource = function() {
-                    return $scope.imageSource || appSettings.defaultCompanyImageUrl;
+                scope.imagePreviewSource = function() {
+                    return scope.imageSource || appSettings.defaultCompanyImageUrl;
                 };
             },
             templateUrl: appSettings.templatesFolderPath + 'image-upload.cshtml'
