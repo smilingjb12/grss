7f0e85cf3585ede7346500a113d7cdfa339e763e
Vitaly Shakh <v.shah@itransition.com>
2015-08-21 19:15:22 +0300

    Added category select directive.

--- GRSS.Web/App/controllers/supplierEditCtrl.js
+++ GRSS.Web/App/controllers/supplierEditCtrl.js
@@ -1,10 +1,20 @@
-﻿(function() {
+﻿(function () {
     "use strict";
 
     angular.module('app')
         .controller('SupplierEditCtrl', ['$scope', SupplierEditCtrl]);
 
     function SupplierEditCtrl($scope) {
-        $scope.companyLegalStatuses = [ 'Ltd', 'Inc', "SSH" ];
+        $scope.companyLegalStatuses = ['Ltd', 'Inc', "SSH"];
+        $scope.numberOfEmployeesSet = ['1-10', '10-50', '50-100', '100-250', '250-1000', '1000-5000', '>5000'];
+        $scope.supplierCompany = { categories: [] };
+
+        $scope.addCategory = function () {
+            $scope.supplierCompany.categories.push({});
+        };
+
+        $scope.removeCategory = function(index) {
+            $scope.supplierCompany.categories.splice(index, 1);
+        };
     }
 })();
---
+++ GRSS.Web/App/directives/categorySelect.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('app')
+        .directive('categorySelect', ['appSettings', CategorySelect]);
+
+    function CategorySelect(appSettings) {
+        return {
+            restrict: 'E',
+            scope: {
+                selectedCategory: '=',
+                'remove': '&onRemove'
+            },
+            controller: function ($scope) {
+                $scope.verticalMarkets = [{ Id: 1, Name: "vm1" }, { Id: 2, Name: "vm2" }, { Id: 3, Name: "vm3" }];
+                $scope.businessUnits = [{ Id: 1, Name: "bu1" }, { Id: 2, Name: "bu2" }, { Id: 3, Name: "bu3" }];
+                $scope.categories = [{ Id: 1, Name: "c1" }, { Id: 2, Name: "c2" }, { Id: 3, Name: "c3" }];
+            },
+            templateUrl: appSettings.viewsFolderPath + 'category-select.cshtml'
+        };
+    }
+})();
